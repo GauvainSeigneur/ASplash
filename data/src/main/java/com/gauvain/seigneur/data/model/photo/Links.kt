e228bf48f3af2402b8766e91fca632a21acb6395
gauvain <gauvain.seigneur@gmail.com>
2022-09-17 12:51:22 +0200

    Photo - rework photo model and sub model

---
+++ data/src/main/java/com/gauvain/seigneur/data/model/photo/Links.kt
@@ -0,0 +1,8 @@
+package com.gauvain.seigneur.data.model.photo
+
+data class Links(
+    var self: String?,
+    var html: String?,
+    var download: String?,
+    var download_location: String?
+)
--- data/src/main/java/com/gauvain/seigneur/data/model/photo/Photo.kt
+++ data/src/main/java/com/gauvain/seigneur/data/model/photo/Photo.kt
@@ -1,10 +1,14 @@
-package com.gauvain.seigneur.data.model
+package com.gauvain.seigneur.data.model.photo
 
 data class Photo(
     val id: String,
     val width : Int,
     val height: Int,
     val color: String?,
+    //var user: User,
+    var urls: PhotoUrl,
+    var exif: Exif?,
+    var links: Links,
     var description: String?,
     var liked_by_user:Boolean
 )
